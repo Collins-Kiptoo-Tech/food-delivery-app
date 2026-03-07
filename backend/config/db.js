6a631b3adfcad6035f694a29e82a121045939c56
Collins Kiptoo <collinskiptoo259@gmail.com>
2026-03-07 10:47:53 +0300

    final commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -10,14 +10,10 @@ import 'dotenv/config';
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("DB Connected");
   } catch (err) {
     console.error("Database connection failed:", err);
     process.exit(1); // Exit the app if DB connection fails
   }
 };
-
